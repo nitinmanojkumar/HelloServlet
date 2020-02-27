80da22b94002b65dabe636dbae3fa1ac764a7fb7
nitinmanojkumar <nitinmanojkumar@gmail.com>
2020-02-27 13:19:39 +0530

    helloServlet

---
+++ src/org/studyeasy/servlet/HelloWorld.java
@@ -0,0 +1,36 @@
+package org.studyeasy.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorld() {
+    	System.out.println("its constructor");
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("hey");
+		response.getWriter().println("<h1>Hello World</h1>");
+		
+	}
+
+}
